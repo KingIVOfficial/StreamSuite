78d3ca014281decb4ced5196b852760f335abad5
KingIVOfficial <arthurpsmith9@gmail.com>
2026-09-16 03:46:38 +0000

    Bind server to Railway network interface

--- server.js
+++ server.js
@@ -185,7 +185,7 @@ app.get('/success/:pageId/:listingSlug', (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`StreamSuite storefront listening on port ${PORT}`);
 });
 
